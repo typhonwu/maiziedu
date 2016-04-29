2cbff65b2b3637e6dac2c381dd6e8c86bb1a401d
hughohoho <johnson_hugh@163.com>
2016-04-29 10:23:41 +0800

    第一次作业：字符串内容统计

---
+++ count.py
@@ -0,0 +1,17 @@
+# coding: UTF-8
+'''
+题目：输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
+程序分析：isalpha方法判断是否是字母，isspace方法判断是否是空格，isdigit方法判断是否是数字
+'''
+scen = raw_input('Enter a scentence:')
+alpha = space = digit = rest = 0
+for s in scen:
+    if str.isalpha(s):alpha +=1
+    elif str.isspace(s):space +=1
+    elif str.isdigit(s):digit +=1
+    else: rest +=1
+
+print('字母个数：'+str(alpha))
+print('空格个数：'+str(space))
+print('数字个数：'+str(digit))
+print('其他个数：'+str(rest))
