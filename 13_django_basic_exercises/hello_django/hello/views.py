7ffd134332bcfe16c211f8947ec3135b821465a3
hughohoho <johnson_hugh@163.com>
2016-05-20 10:51:45 +0800

    在模板中加入数据库的用户信息

--- 13_django_basic_exercises/hello_django/hello/views.py
+++ 13_django_basic_exercises/hello_django/hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
 def hello(request):
-    return render(request,'table.html')
+	user_list = User.objects.all()
+	return render(request,'table.html',{'user_list':user_list})
     
