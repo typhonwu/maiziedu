ccd79558dea5dce7636fff05eb2c209517a94d59
hughohoho <johnson_hugh@163.com>
2016-05-23 13:48:52 +0800

    通过meta扩展属性把表名改成中文的

--- 13_django_basic_exercises/hello_django/hello/models.py
+++ 13_django_basic_exercises/hello_django/hello/models.py
@@ -17,8 +17,15 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     website = models.URLField()
 
+    class Meta:
+        verbose_name = '出版商'
+        verbose_name_plural = verbose_name
 class Author(models.Model):
     name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = '作者'
+        verbose_name_plural = verbose_name
 
 class AuthorDetail(models.Model):
     sex = models.BooleanField(max_length=1, choices=((0, '男'),(1, '女'),))
@@ -27,8 +34,16 @@ class AuthorDetail(models.Model):
     birthday = models.DateField()
     author = models.OneToOneField(Author)
 
+    class Meta:
+        verbose_name = '作者细节'
+        verbose_name_plural = verbose_name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
+
+    class Meta:
+        verbose_name = '书籍'
+        verbose_name_plural = verbose_name
