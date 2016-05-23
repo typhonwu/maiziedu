01f4982405fb1bb8af3b3807f3a1c8c69ba2765c
hughohoho <johnson_hugh@163.com>
2016-05-23 12:16:48 +0800

    配置了mysql数据库

--- 13_django_basic_exercises/hello_django/hello_django/settings.py
+++ 13_django_basic_exercises/hello_django/hello_django/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'hello_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hello_django_db',
+        'USER':'root',
+        'PASSWORD':'123456',
+        #'HOST':'',
+        #'PORT':'',
     }
 }
 
