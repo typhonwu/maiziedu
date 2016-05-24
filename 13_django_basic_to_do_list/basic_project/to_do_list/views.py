e233e22942aba4a75b4231dc79c5ca8fe0a2bfff
hughohoho <johnson_hugh@163.com>
2016-05-24 19:28:50 +0800

    第十三次作业-业务逻辑-把views中的框架和urls中的映射搭建好

--- 13_django_basic_to_do_list/basic_project/to_do_list/views.py
+++ 13_django_basic_to_do_list/basic_project/to_do_list/views.py
@@ -1,5 +1,19 @@
+# coding:utf-8
 from django.shortcuts import render
 
 # Create your views here.
+#待办事项列表
 def index(request):
 	return render(request,'index.html',locals())
+#添加待办事项,需要对列表进行分页
+def add(request):
+    pass
+#修改待办事项
+def edit(request):
+    pass
+#删除待办事项
+def delete(request):
+    pass
+#标记事项完成
+def done(request):
+    pass
