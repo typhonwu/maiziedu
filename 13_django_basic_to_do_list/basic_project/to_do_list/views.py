3070d34f446753d9d0cde9d7f68f041be5e6d6ee
hughohoho <johnson_hugh@163.com>
2016-05-25 15:53:53 +0800

    第十四次作业-个人博客-创建项目和app之后配置settings

--- 13_django_basic_to_do_list/basic_project/to_do_list/views.py
+++ 13_django_basic_to_do_list/basic_project/to_do_list/views.py
@@ -7,8 +7,8 @@ from django.core.paginator import Paginator,PageNotAnInteger,InvalidPage,EmptyPa
 def index(request):
     try:
         item_list = Item.objects.all().order_by("-pub_date")
-        #把查询的结果每五条一页分页
-        paginator = Paginator(item_list,5)
+        #把查询的结果每三条一页分页
+        paginator = Paginator(item_list,3)
         try:
             #获取页码,如果没取到就默认第一页
             page = int(request.GET.get("page",1))
