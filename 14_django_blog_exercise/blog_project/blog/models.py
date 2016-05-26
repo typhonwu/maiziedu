19e13c5b1fdeab2d5db254da77818d14904cf56a
hughohoho <johnson_hugh@163.com>
2016-05-26 11:43:41 +0800

    第十四次作业-个人博客-模型设计-用户-继承方式

--- 14_django_blog_exercise/blog_project/blog/models.py
+++ 14_django_blog_exercise/blog_project/blog/models.py
@@ -29,3 +29,23 @@ class Category(models.Model):
 
     def __unicode__(self):
         return self.name
+
+# 用户模型
+# 继承方式扩展AbstractUser，可以使用django封装好的权限等属性
+# 扩展：还可以用关联方式扩展用户信息，one to one
+# 两者各有优劣
+class User(AbstractUser):
+	#头像用图片类型
+    avatar = models.ImageField(upload_to='avatar/%Y/%m', default='avatar/default.png', max_length=200, blank=True, null=True, verbose_name='用户头像')
+    #可以为空
+    qq = models.CharField(max_length=20, blank=True, null=True, verbose_name='QQ号码')
+    #不能重复
+    mobile = models.CharField(max_length=11, blank=True, null=True, unique=True, verbose_name='手机号码')
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+        ordering = ['-id']
+
+    def __unicode__(self):
+        return self.username
