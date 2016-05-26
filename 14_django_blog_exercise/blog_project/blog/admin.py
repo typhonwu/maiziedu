64c73817eaeb61ada827b0edcea58413d2271152
hughohoho <johnson_hugh@163.com>
2016-05-26 17:18:57 +0800

    第十四次作业-个人博客-admin-将所有model注册到blog下的admin.py中，这样在admin后台就能看到这些数据表了

--- 14_django_blog_exercise/blog_project/blog/admin.py
+++ 14_django_blog_exercise/blog_project/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from blog.models import *
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Links)
+admin.site.register(Ad)
