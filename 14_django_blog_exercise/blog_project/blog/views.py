fd0594a03aca91a30e358e70da828a159be29244
hughohoho <johnson_hugh@163.com>
2016-05-26 09:51:35 +0800

    第十四次作业-个人博客-学习日志器的配置与使用，在setting中配置好之后主要在views.py中调用

--- 14_django_blog_exercise/blog_project/blog/views.py
+++ 14_django_blog_exercise/blog_project/blog/views.py
@@ -1,6 +1,14 @@
+# -*- coding:utf-8 -*-
 from django.shortcuts import render
-
+import logging
+#使用setting.py中配置的日志器，一般都在views.py中使用日志器，因为这里都是业务逻辑
+logger = logging.getLogger('blog.views')
 # Create your views here.
 #定义首页方法
 def index(request):
+    try:
+        file = open('sss.txt','r')
+    except Exception as e:
+        #如果出现异常就写入日志
+        logger.error(e)
     return render(request,'index.html',locals())
