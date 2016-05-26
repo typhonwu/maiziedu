a3475a31c0609dfffb90fedcd488eb9cb573a34b
hughohoho <johnson_hugh@163.com>
2016-05-26 17:37:45 +0800

    第十四次作业-个人博客-admin-自定义展开与合并显示

--- 14_django_blog_exercise/blog_project/blog/admin.py
+++ 14_django_blog_exercise/blog_project/blog/admin.py
@@ -1,9 +1,22 @@
+# -*- utf-8 -*-
 from django.contrib import admin
 from blog.models import *
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-	#决定后台只显示哪些字段
-	exclude = ('title','desc','content',)
+	#决定哪些是展开显示，哪些合并在一起
+	fieldsets = (
+		#这些展开
+		(None, {
+				'fields': ('title','desc','content',)
+			}
+		),
+		#这些默认合并
+		('高级设置',{
+				'classes':('collapse',),
+				'fields':('click_count','is_recommend',)
+			}
+		),
+	)
 
 admin.site.register(User)
 admin.site.register(Tag)
