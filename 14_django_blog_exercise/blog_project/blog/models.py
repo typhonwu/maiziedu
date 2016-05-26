3283d40dce70dd5b4670547ea52ae895f736f799
hughohoho <johnson_hugh@163.com>
2016-05-26 11:36:37 +0800

    第十四次作业-个人博客-模型设计-标签

--- 14_django_blog_exercise/blog_project/blog/models.py
+++ 14_django_blog_exercise/blog_project/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+	name = models.CharFiels(max_length = 30,verbose_name='标签名称')
+
+	#方便在admin中查看名字
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+        ordering = ['id']
+
+    #调用时返回自身的一些属性，注意python3用这个，python2用__unicode__
+    def __str__(self):
+    	#必须返回字符串类型，str(self.id)
+        return self.name
