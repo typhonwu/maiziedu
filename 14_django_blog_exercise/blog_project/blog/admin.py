a677164d4faf4f9f989864a25cd4367f8c911ba1
hughohoho <johnson_hugh@163.com>
2016-05-26 17:26:42 +0800

    第十四次作业-个人博客-admin-自定义显示哪些字段

--- 14_django_blog_exercise/blog_project/blog/admin.py
+++ 14_django_blog_exercise/blog_project/blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from blog.models import *
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+	#决定后台只显示哪些字段
+	fields = ('title','desc','content',)
+
 admin.site.register(User)
 admin.site.register(Tag)
-admin.site.register(Article)
+#改成自定义形式注册
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Links)
