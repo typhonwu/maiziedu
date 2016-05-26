4d49a11088d4a1c684a290c5e5705470b48902a6
hughohoho <johnson_hugh@163.com>
2016-05-26 11:46:18 +0800

    第十四次作业-个人博客-模型设计-文章

--- 14_django_blog_exercise/blog_project/blog/models.py
+++ 14_django_blog_exercise/blog_project/blog/models.py
@@ -49,3 +49,24 @@ class User(AbstractUser):
 
     def __unicode__(self):
         return self.username
+
+# 文章模型
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name='文章标题')
+    desc = models.CharField(max_length=50, verbose_name='文章描述')
+    content = models.TextField(verbose_name='文章内容')
+    click_count = models.IntegerField(default=0, verbose_name='点击次数')
+    is_recommend = models.BooleanField(default=False, verbose_name='是否推荐')
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+    user = models.ForeignKey(User, verbose_name='用户')
+    #外键
+    category = models.ForeignKey(Category, blank=True, null=True, verbose_name='分类')
+    tag = models.ManyToManyField(Tag, verbose_name='标签')
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+        ordering = ['-date_publish']
+
+    def __unicode__(self):
+        return self.title
