039cb8682077296cc760fac17faf8e5698c9184c
hughohoho <johnson_hugh@163.com>
2016-05-26 15:37:18 +0800

    第十四次作业-个人博客-数据库配置-记得配置__init__.py，在其中导入pymysql模块，另外就是缩进要用tab，空格是不认的

--- 14_django_blog_exercise/blog_project/blog/models.py
+++ 14_django_blog_exercise/blog_project/blog/models.py
@@ -1,35 +1,35 @@
 # -*- coding:utf-8 -*-
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Tag(models.Model):
-	name = models.CharFiels(max_length = 30,verbose_name='标签名称')
+	name = models.CharField(max_length = 30,verbose_name='标签名称')
 
 	#方便在admin中查看名字
-    class Meta:
-        verbose_name = '标签'
-        verbose_name_plural = verbose_name
-        ordering = ['id']
+	class Meta:
+		verbose_name = '标签'
+		verbose_name_plural = verbose_name
+		ordering = ['id']
 
-    #调用时返回自身的一些属性，注意python3用这个，python2用__unicode__
-    def __str__(self):
-    	#必须返回字符串类型，str(self.id)
-        return self.name
+	#调用时返回自身的一些属性，注意python3用这个，python2用__unicode__
+	def __str__(self):
+		#必须返回字符串类型，str(self.id)
+		return self.name
 
 # 文章分类
 class Category(models.Model):
-    name = models.CharField(max_length=30, verbose_name='分类名称')
-    #排序属性
-    index = models.IntegerField('显示顺序(从小到大)',default=999,verbose_name='分类的排序')
+	name = models.CharField(max_length=30, verbose_name='分类名称')
+	#排序属性
+	index = models.IntegerField('显示顺序(从小到大)',default=999)
 
-    class Meta:
-        verbose_name = '分类'
-        verbose_name_plural = verbose_name
-        #按照index和id属性排序
-        ordering = ['index', 'id']
+	class Meta:
+		verbose_name = '分类'
+		verbose_name_plural = verbose_name
+		#按照index和id属性排序
+		ordering = ['index', 'id']
 
-    def __unicode__(self):
-        return self.name
+	def __unicode__(self):
+		return self.name
 
 # 用户模型
 # 继承方式扩展AbstractUser，可以使用django封装好的权限等属性
@@ -37,88 +37,88 @@ class Category(models.Model):
 # 两者各有优劣
 class User(AbstractUser):
 	#头像用图片类型
-    avatar = models.ImageField(upload_to='avatar/%Y/%m', default='avatar/default.png', max_length=200, blank=True, null=True, verbose_name='用户头像')
-    #可以为空
-    qq = models.CharField(max_length=20, blank=True, null=True, verbose_name='QQ号码')
-    #不能重复
-    mobile = models.CharField(max_length=11, blank=True, null=True, unique=True, verbose_name='手机号码')
+	avatar = models.ImageField(upload_to='avatar/%Y/%m', default='avatar/default.png', max_length=200, blank=True, null=True, verbose_name='用户头像')
+	#可以为空
+	qq = models.CharField(max_length=20, blank=True, null=True, verbose_name='QQ号码')
+	#不能重复
+	mobile = models.CharField(max_length=11, blank=True, null=True, unique=True, verbose_name='手机号码')
 
-    class Meta:
-        verbose_name = '用户'
-        verbose_name_plural = verbose_name
-        ordering = ['-id']
+	class Meta:
+		verbose_name = '用户'
+		verbose_name_plural = verbose_name
+		ordering = ['-id']
 
-    def __unicode__(self):
-        return self.username
+	def __unicode__(self):
+		return self.username
 
 # 文章模型
 class Article(models.Model):
-    title = models.CharField(max_length=50, verbose_name='文章标题')
-    desc = models.CharField(max_length=50, verbose_name='文章描述')
-    content = models.TextField(verbose_name='文章内容')
-    click_count = models.IntegerField(default=0, verbose_name='点击次数')
-    is_recommend = models.BooleanField(default=False, verbose_name='是否推荐')
-    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
-    user = models.ForeignKey(User, verbose_name='用户')
-    #外键
-    category = models.ForeignKey(Category, blank=True, null=True, verbose_name='分类')
-    #多对多关系将会自动多生成一张表，blog_article_tag
-    tag = models.ManyToManyField(Tag, verbose_name='标签')
+	title = models.CharField(max_length=50, verbose_name='文章标题')
+	desc = models.CharField(max_length=50, verbose_name='文章描述')
+	content = models.TextField(verbose_name='文章内容')
+	click_count = models.IntegerField(default=0, verbose_name='点击次数')
+	is_recommend = models.BooleanField(default=False, verbose_name='是否推荐')
+	date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+	user = models.ForeignKey(User, verbose_name='用户')
+	#外键
+	category = models.ForeignKey(Category, blank=True, null=True, verbose_name='分类')
+	#多对多关系将会自动多生成一张表，blog_article_tag
+	tag = models.ManyToManyField(Tag, verbose_name='标签')
 
-    class Meta:
-        verbose_name = '文章'
-        verbose_name_plural = verbose_name
-        ordering = ['-date_publish']
+	class Meta:
+		verbose_name = '文章'
+		verbose_name_plural = verbose_name
+		ordering = ['-date_publish']
 
-    def __unicode__(self):
-        return self.title
+	def __unicode__(self):
+		return self.title
 
 # 评论模型
 class Comment(models.Model):
-    content = models.TextField(verbose_name='评论内容')
-    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
-    user = models.ForeignKey(User, blank=True, null=True, verbose_name='用户')
-    article = models.ForeignKey(Article, blank=True, null=True, verbose_name='文章')
-    pid = models.ForeignKey('self', blank=True, null=True, verbose_name='父级评论')
+	content = models.TextField(verbose_name='评论内容')
+	date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+	user = models.ForeignKey(User, blank=True, null=True, verbose_name='用户')
+	article = models.ForeignKey(Article, blank=True, null=True, verbose_name='文章')
+	pid = models.ForeignKey('self', blank=True, null=True, verbose_name='父级评论')
 
-    class Meta:
-        verbose_name = '评论'
-        verbose_name_plural = verbose_name
-        #按发布日期倒序排列
-        ordering = ['-date_publish']
+	class Meta:
+		verbose_name = '评论'
+		verbose_name_plural = verbose_name
+		#按发布日期倒序排列
+		ordering = ['-date_publish']
 
-    def __unicode__(self):
-        return str(self.id)
+	def __unicode__(self):
+		return str(self.id)
 
 # 友情链接
 class Links(models.Model):
-    title = models.CharField(max_length=50, verbose_name='标题')
-    description = models.CharField(max_length=200, verbose_name='友情链接描述')
-    callback_url = models.URLField(verbose_name='url地址')
-    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
-    index = models.IntegerField(default=999, verbose_name='排列顺序(从小到大)')
+	title = models.CharField(max_length=50, verbose_name='标题')
+	description = models.CharField(max_length=200, verbose_name='友情链接描述')
+	callback_url = models.URLField(verbose_name='url地址')
+	date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+	index = models.IntegerField(default=999, verbose_name='排列顺序(从小到大)')
 
-    class Meta:
-        verbose_name = '友情链接'
-        verbose_name_plural = verbose_name
-        ordering = ['index', 'id']
+	class Meta:
+		verbose_name = '友情链接'
+		verbose_name_plural = verbose_name
+		ordering = ['index', 'id']
 
-    def __unicode__(self):
-        return self.title
+	def __unicode__(self):
+		return self.title
 
 # 广告
 class Ad(models.Model):
-    title = models.CharField(max_length=50, verbose_name='广告标题')
-    description = models.CharField(max_length=200,  verbose_name='广告描述')
-    image_url = models.ImageField(upload_to='ad/%Y/%m', verbose_name='图片路径')
-    callback_url = models.URLField(null=True, blank=True, verbose_name='回调url')
-    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
-    index = models.IntegerField(default=999, verbose_name='排列顺序(从小到大)')
+	title = models.CharField(max_length=50, verbose_name='广告标题')
+	description = models.CharField(max_length=200,  verbose_name='广告描述')
+	image_url = models.ImageField(upload_to='ad/%Y/%m', verbose_name='图片路径')
+	callback_url = models.URLField(null=True, blank=True, verbose_name='回调url')
+	date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+	index = models.IntegerField(default=999, verbose_name='排列顺序(从小到大)')
 
-    class Meta:
-        verbose_name = u'广告'
-        verbose_name_plural = verbose_name
-        ordering = ['index', 'id']
+	class Meta:
+		verbose_name = u'广告'
+		verbose_name_plural = verbose_name
+		ordering = ['index', 'id']
 
-    def __unicode__(self):
-        return self.title
+	def __unicode__(self):
+		return self.title
