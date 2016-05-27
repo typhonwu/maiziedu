0831ddacc1bf2716ea931b5a562b31a354dae9cf
hughohoho <johnson_hugh@163.com>
2016-05-27 14:43:33 +0800

    第十四次作业-个人博客-重构代码-第一步-重构 类别，广告，归档，标签云，友情链接，文章排行这些公用模块，放到django预先准备的global_setting中

--- 14_django_blog_exercise/blog_project/blog/views.py
+++ 14_django_blog_exercise/blog_project/blog/views.py
@@ -9,16 +9,23 @@ from django.core.paginator import Paginator,InvalidPage,EmptyPage,PageNotAnInteg
 logger = logging.getLogger('blog.views')
 #用setting数据定义全局变量,返回一个字典
 def global_setting(request):
+    #重构一：把类别，广告，归档这些公用内容提出来
+    category_list = Category.objects.all()
+    ad_list = Ad.objects.all()[:5]
+    archive_list = Article.objects.distinct_date()
+    #标签云数据
+    #友情链接数据
+    #文章排行榜数据
     return {'SITE_NAME':settings.SITE_NAME,
-            'SITE_DESC':settings.SITE_DESC}
+            'SITE_DESC':settings.SITE_DESC,
+            'category_list':category_list,
+            'archive_list':archive_list,
+            'ad_list':ad_list,
+           }
 # Create your views here.
 #定义首页方法
 def index(request):
     try:
-        #分类信息获取（导航数据）
-        category_list = Category.objects.all()
-        #广告数据
-        ad_list = Ad.objects.all()[:5]
         #最新文章数据
         article_list = Article.objects.all()
         paginator = Paginator(article_list,10)
@@ -29,17 +36,12 @@ def index(request):
         except (EmptyPage,InvalidPage,PageNotAnInteger):
             #如果出错默认返回第一页
             article_list = paginator.page(1)
-        #添加归档方法
-        archive_list = Article.objects.distinct_date()
     except Exception as e:
         #如果出现异常就写入日志
         logger.error(e)
     return render(request,'index.html',locals())
 def archive(request):
     try:
-        category_list = Category.objects.all()
-        ad_list = Ad.objects.all()[:5]
-        archive_list = Article.objects.distinct_date()
         #先获取客户端提交的信息
         year = request.GET.get('year',None)
         month = request.GET.get('month',None)
