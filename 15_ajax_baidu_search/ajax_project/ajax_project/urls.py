61acf5faec6cd24522da7ff784aed99dc2ffac4a
hughohoho <johnson_hugh@163.com>
2016-05-30 23:25:18 +0800

    第十五次作业-ajax百度搜索框提示-用jquery的ajax函数发送请求到django，同时在django设置了url映射autoComplete,在views中获取传递的查询参数，用__contains查询数据库，把返回的列表转成json array类型，返回HttpResponse-还差如何对回应的json数据进行处理

--- 15_ajax_baidu_search/ajax_project/ajax_project/urls.py
+++ 15_ajax_baidu_search/ajax_project/ajax_project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from baidu_search.views import autoComplete
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^autocomplete$', autoComplete, name='autoCompelete'),
 ]
--- 15_ajax_baidu_search/ajax_project/baidu_search/views.py
+++ 15_ajax_baidu_search/ajax_project/baidu_search/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from baidu_search.models import Item
+import json
 # Create your views here.
+def autoComplete(request):
+    query = request.GET.get('query',None)
+    if query:
+        item_list = Item.objects.filter(title__contains=query)
+        # 列表将会被转化为json数组
+        result = json.dumps(item_list)
+    return HttpResponse(result,content_type="application/json")
