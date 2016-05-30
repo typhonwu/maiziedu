2e35e527671c7bbb0ccb25041cbfac42c8d9c6b0
hughohoho <johnson_hugh@163.com>
2016-05-30 15:59:59 +0800

    第十五次作业-ajax百度搜索框提示-为搜索记录建立模型，记得把app注册到settings中，否则数据库迁移时不会扫描到

--- 15_ajax_baidu_search/ajax_project/baidu_search/models.py
+++ 15_ajax_baidu_search/ajax_project/baidu_search/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=100,verbose_name='搜索记录')
+
+    class Meta:
+        verbose_name = u'搜索记录'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
