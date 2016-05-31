d45a897f31d386d9a2cd4ee8024a78b0bf3d2020
hughohoho <johnson_hugh@163.com>
2016-05-31 10:34:13 +0800

    第十五次作业-ajax百度搜索框提示-大体的代码都完成了，使用了django自带的JsonResponse模块，返回json数据更方便。就是还没成功，问老师等消息

--- 15_ajax_baidu_search/ajax_project/baidu_search/views.py
+++ 15_ajax_baidu_search/ajax_project/baidu_search/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from baidu_search.models import Item
-import json
+from django.http import JsonResponse
 # Create your views here.
 def index(request):
     return render(request,'baidu_search.html')
@@ -8,10 +8,5 @@ def autoComplete(request):
     query = request.GET.get('query',None)
     if query:
         item_list = Item.objects.filter(title__contains=query)
-        # 列表将会被转化为json数组
-        result = json.dumps(item_list)
-        print (result)
-        with open('json_test.txt','a') as f:
-            f.write(result)
-
-    return HttpResponse(result,content_type="application/json")
+    # 列表将会被转化为json数组
+    return JsonResponse(item_list,safe=False)
