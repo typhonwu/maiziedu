b14460dd0cbd8e3580857e9d06af81d0e9fa49d8
hughohoho <johnson_hugh@163.com>
2016-06-01 13:05:25 +0800

    第十五次作业-ajax百度搜索框提示-教程-用分页器实现每次显示四条记录

--- 15_ajax_baidu_search/ajax_project/baidu_search/views.py
+++ 15_ajax_baidu_search/ajax_project/baidu_search/views.py
@@ -4,6 +4,7 @@ from baidu_search.models import Item
 from django.http import JsonResponse,HttpResponse
 from django.core.serializers import serialize
 import json
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     return render(request,'baidu_search_tutorial.html')
@@ -20,9 +21,11 @@ def search(req):
     try:
         if word:
             #区别contains和icontains，后者忽略大小写
-            item_list = Item.objects.filter(title__icontains=word)[:4]
+            #考虑到实现效果4条一页，这里也这样分页
+            paginator = Paginator(Item.objects.filter(title__icontains=word),4)
             #这里把列表序列化，然后取名为json，二者形成键值对的json对象，传给word_list
-            word_list = serialize('json',item_list)
+            #先返回第一页的四条记录
+            word_list = serialize('json',paginator.page(1))
             #这里不需要返回模板，返回json即可,需要把它用dumps打包一下，指明类型
     except:
         pass
