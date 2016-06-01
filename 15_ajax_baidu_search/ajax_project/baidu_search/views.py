d47de9df9fb93bd09ea00cff6632193e24a108f7
hughohoho <johnson_hugh@163.com>
2016-06-01 14:51:13 +0800

    第十五次作业-ajax百度搜索框提示-学了教程之后把自己那个结合autocomplete插件做的实现了，有个问题就是下拉框会很长，还重复，应该是加入数据源的操作还要优化一下

--- 15_ajax_baidu_search/ajax_project/baidu_search/views.py
+++ 15_ajax_baidu_search/ajax_project/baidu_search/views.py
@@ -7,7 +7,7 @@ import json
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    return render(request,'baidu_search_tutorial.html')
+    return render(request,'baidu_search.html')
 def autoComplete(request):
     query = request.GET.get('query',None)
     if query:
@@ -17,17 +17,7 @@ def autoComplete(request):
 
 def search(req):
     word = req.GET.get('word','')
-    print (word)
-    try:
-        if word:
-            #区别contains和icontains，后者忽略大小写
-            #考虑到实现效果4条一页，这里也这样分页
-            paginator = Paginator(Item.objects.filter(title__icontains=word),4)
-            #这里把列表序列化，然后取名为json，二者形成键值对的json对象，传给word_list
-            #先返回第一页的四条记录
-            word_list = serialize('json',paginator.page(1))
-            #这里不需要返回模板，返回json即可,需要把它用dumps打包一下，指明类型
-    except:
-        pass
+    paginator = Paginator(Item.objects.filter(title__icontains=word),4)
+    word_list = serialize('json',paginator.page(1))
     return HttpResponse(json.dumps(word_list),content_type='application/json')
   
