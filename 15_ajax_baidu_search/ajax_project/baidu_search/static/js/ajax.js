fb27c808d7b32ad58d6ef14c1044122a2905bbbc
hughohoho <johnson_hugh@163.com>
2016-06-01 12:38:41 +0800

    第十五次作业-ajax百度搜索框提示-教程-在前端jquery中把获取的值用$.parseJSON包装成js对象之后再用$.each遍历出来

--- 15_ajax_baidu_search/ajax_project/baidu_search/static/js/ajax.js
+++ 15_ajax_baidu_search/ajax_project/baidu_search/static/js/ajax.js
@@ -4,7 +4,14 @@ function do_ajax(){
     //用键值对的形式向服务器传送数据，在django的views中也用相同名称获取数据
     //这个方法就是$.ajax()的简写形式
     $.get('/search/', {'word':word}, function(data){
-        alert(data.length);
+        //alert(data.length);
+        //获得的data里面其实是一个字典，用each方法遍历出来
+        //jQuery.parseJSON()函数用于将格式完好的JSON字符串转为与之对应的JavaScript对象。
+        queries = $.parseJSON(data)
+        $.each(queries,function(key,value){
+            //具体取哪个字段可以在chrome的控制台查看一下返回的json文件内容
+            $('#word_txt').html('<li>'+value.fields.title+'</li>')
+        });
     });
 }
 
