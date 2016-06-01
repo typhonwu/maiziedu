b0665add916933d869c694681acc97e0aad80826
hughohoho <johnson_hugh@163.com>
2016-06-01 12:49:37 +0800

    第十五次作业-ajax百度搜索框提示-教程-把显示返回数据的div块显示出来

--- 15_ajax_baidu_search/ajax_project/baidu_search/static/js/ajax.js
+++ 15_ajax_baidu_search/ajax_project/baidu_search/static/js/ajax.js
@@ -1,6 +1,7 @@
 //get或post方法
 function do_ajax(){
     word = $("#word").val();
+    $('#drop_down').css('display','block');
     //用键值对的形式向服务器传送数据，在django的views中也用相同名称获取数据
     //这个方法就是$.ajax()的简写形式
     $.get('/search/', {'word':word}, function(data){
