e89eb3d9fc0148b371abcddc9812b34af1b19eed
hughohoho <johnson_hugh@163.com>
2016-06-01 12:56:00 +0800

    第十五次作业-ajax百度搜索框提示-教程-把实现显示多条记录

--- 15_ajax_baidu_search/ajax_project/baidu_search/static/js/ajax.js
+++ 15_ajax_baidu_search/ajax_project/baidu_search/static/js/ajax.js
@@ -1,7 +1,6 @@
 //get或post方法
 function do_ajax(){
     word = $("#word").val();
-    $('#drop_down').css('display','block');
     //用键值对的形式向服务器传送数据，在django的views中也用相同名称获取数据
     //这个方法就是$.ajax()的简写形式
     $.get('/search/', {'word':word}, function(data){
@@ -9,9 +8,20 @@ function do_ajax(){
         //获得的data里面其实是一个字典，用each方法遍历出来
         //jQuery.parseJSON()函数用于将格式完好的JSON字符串转为与之对应的JavaScript对象。
         queries = $.parseJSON(data)
+        str = ''
         $.each(queries,function(key,value){
             //具体取哪个字段可以在chrome的控制台查看一下返回的json文件内容
-            $('#word_txt').html('<li>'+value.fields.title+'</li>')
+            //这种写法每次都会刷新，最后只会出现最后一条记录
+            //$('#word_txt').html('<li>'+value.fields.title+'</li>')
+            str += '<li>'+value.fields.title+'</li>';
+            if(str != ''){
+                $('#drop_down').css('display','block');
+                $('#word_txt').html(str)
+            }
+            else{
+                $('#drop_down').css('display','none');
+            }
+            
         });
     });
 }
