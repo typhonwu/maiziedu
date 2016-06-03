28a812efbb18665ffd396b239aac306a83437cd8
hughohoho <johnson_hugh@163.com>
2016-06-03 14:44:59 +0800

    第十六次作业-django高级-自定义过滤器-装饰器注册

--- 13_django_basic_to_do_list/basic_project/to_do_list/templatetags/to_do_filter.py
+++ 13_django_basic_to_do_list/basic_project/to_do_list/templatetags/to_do_filter.py
@@ -3,9 +3,11 @@ from django import template
 register = template.Library()
 
 # 定义一个过滤器用于去掉空格
+# 可以用装饰器注册
+@register.filter()
 def cut_filter(value, arg):
 	return value.replace(arg, '')
 
 
 # 注册这个自定义过滤器，名字可以随便起，主要是指定调用哪个函数
-register.filter(name="cut_filter", filter_func=cut_filter)
+# register.filter(name="cut_filter", filter_func=cut_filter)
