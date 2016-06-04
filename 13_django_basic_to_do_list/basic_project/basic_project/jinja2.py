96f351860f404795eed26679547c896d5ab01b7d
hughohoho <johnson_hugh@163.com>
2016-06-04 09:42:59 +0800

    第十六次作业-django高级-用jinja2替换django原生模板-过滤器-可以自定义过滤器，在jinjia2.py中加入。-另外还可以自定义标签，不过叫extension扩展

--- 13_django_basic_to_do_list/basic_project/basic_project/jinja2.py
+++ 13_django_basic_to_do_list/basic_project/basic_project/jinja2.py
@@ -1,6 +1,8 @@
 from jinja2 import Environment
-
+from to_do_list.templatetags.to_do_filter import lower
 
 def environment(**options):
     env = Environment(**options)
+    # 在jinjia2中加入自定义过滤器，类似于django中的注册
+    env.filters['allen_lower'] = lower
     return env
