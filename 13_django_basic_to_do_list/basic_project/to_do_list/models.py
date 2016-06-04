24669e101403face2ab6306613ac1c47d428d098
hughohoho <johnson_hugh@163.com>
2016-06-04 10:15:22 +0800

    第十六次作业-django高级-自定义管理器-可以把多个相关操作封装在一个主题的管理器中

--- 13_django_basic_to_do_list/basic_project/to_do_list/models.py
+++ 13_django_basic_to_do_list/basic_project/to_do_list/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 # Create your models here.
 # 这个自定义的管理器就把一些数据操作直接包装在里面了，所以管理器其实就是用来包装复杂的数据处理
-class IncompleteTodoManager(models.Manager)
-    def get_queryset(self):
-        return super(IncompleteTodoManager,self).get_queryset().filter(is_done=0)
+# 可以把多个相关操作封装在一个主题的管理器中
+class TodoManager(models.Manager)
+    def incomplete(self):
+        return self.filter(is_done=False)
 
 class Item(models.Model):
     content = models.CharField(max_length=100,verbose_name=u"待办事项")
@@ -21,3 +22,5 @@ class Item(models.Model):
     # 另外指定Manager管理器，可以有多个
     todoList = models.Manager()
     incomplete = IncompleteTodoManager()
+    # 具体调用形式就变成了Item.objects.incomplete()
+    objects = TodoManager()
