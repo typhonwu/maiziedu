63bdd7d3dc00ef3bbfed8eb081a771526ab88345
hughohoho <johnson_hugh@163.com>
2016-06-04 10:10:37 +0800

    第十六次作业-django高级-自定义管理器-可以有多个

--- 13_django_basic_to_do_list/basic_project/to_do_list/models.py
+++ 13_django_basic_to_do_list/basic_project/to_do_list/models.py
@@ -2,6 +2,11 @@
 from django.db import models
 
 # Create your models here.
+# 这个自定义的管理器就把一些数据操作直接包装在里面了，所以管理器其实就是用来包装复杂的数据处理
+class IncompleteTodoManager(models.Manager)
+    def get_queryset(self):
+        return super(IncompleteTodoManager,self).get_queryset().filter(is_done=0)
+
 class Item(models.Model):
     content = models.CharField(max_length=100,verbose_name=u"待办事项")
     is_done = models.BooleanField(default=False,verbose_name=u"事项状态")
@@ -13,3 +18,6 @@ class Item(models.Model):
     #python3用这个返回信息，python2用__unicode__
     def __str__(self):
         return self.content
+    # 另外指定Manager管理器，可以有多个
+    todoList = models.Manager()
+    incomplete = IncompleteTodoManager()
