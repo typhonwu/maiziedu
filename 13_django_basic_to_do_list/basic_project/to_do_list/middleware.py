7b59f998be6a51eaf0cfa9d2c3bc3704dda63a6d
hughohoho <johnson_hugh@163.com>
2016-06-06 13:01:05 +0800

    第十六次作业-dj给todolist加中间件和过滤器--在定义中间件打印浏览器信息

--- 13_django_basic_to_do_list/basic_project/to_do_list/middleware.py
+++ 13_django_basic_to_do_list/basic_project/to_do_list/middleware.py
@@ -3,4 +3,5 @@ class CheckBrowser(object):
     # 之后，但仍未解析URL以确定应当运行的 view 之前。Django 向它传入相应的
     # HttpRequest 对象，以便在方法中修改。
     def process_request(self,request):
-        print ("aaa")
+        # 将会在后台返回一串信息，包含浏览器信息，这里指定打出浏览器信息
+        print (request.META['HTTP_USER_AGENT']) 
