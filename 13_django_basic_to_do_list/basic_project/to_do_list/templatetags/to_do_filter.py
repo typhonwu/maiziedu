f45b81596c64159536a7e3fd625065f6918d14b3
hughohoho <johnson_hugh@163.com>
2016-06-06 15:22:52 +0800

    第十六次作业-dj给todolist加中间件和过滤器--然后自定义过滤器改为显示过去了多少时间

--- 13_django_basic_to_do_list/basic_project/to_do_list/templatetags/to_do_filter.py
+++ 13_django_basic_to_do_list/basic_project/to_do_list/templatetags/to_do_filter.py
@@ -1,7 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
-
+#声明标签库，方便后面的注册
 register = template.Library()
 
 # 定义一个过滤器用于去掉空格
@@ -19,7 +19,14 @@ def cut_filter(value, arg):
 
 def lower(value):
 	return value.lower()
-
+# 装饰器写法就不需要指定调用哪个函数了
 @register.filter(is_safe=True)
 def add(value, arg):
 	return mark_safe("%s %s" %(value, arg)) 
+
+def mytimesince_filter(value,arg=None):
+	print (value)
+	print (arg)
+	return value
+# 注册这个自定义过滤器，名字可以随便起，主要是指定调用哪个函数
+register.filter("mytimesince",mytimesince_filter)
