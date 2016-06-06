866a995bb78c6a8c3e4711675e66b283cdef3db0
hughohoho <johnson_hugh@163.com>
2016-06-06 12:54:46 +0800

    第十六次作业-dj给todolist加中间件和过滤器-自定义过滤器并注册

--- 13_django_basic_to_do_list/basic_project/basic_project/settings.py
+++ 13_django_basic_to_do_list/basic_project/basic_project/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'to_do_list.middleware.CheckBrowser',
 ]
 
 ROOT_URLCONF = 'basic_project.urls'
