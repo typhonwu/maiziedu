1ac1db92fcb952e1034806ba0cbb732197a42a61
hughohoho <johnson_hugh@163.com>
2016-06-14 18:43:12 +0800

    爬虫基础-urllib2-定制请求头，用urllib2的request对象来发送请求

--- 5_spider_basic/urllib2_demo.py
+++ 5_spider_basic/urllib2_demo.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 '''
-urllib2比urllib多了requests，定制更加丰富
+urllib2比urllib多了urllib2.Request对象，请求头定制更加丰富
 urllib独有编码解码功能
 '''
 import urllib2
@@ -15,5 +15,14 @@ def urlopen():
         print s.read(100)
         s.close()
 
+def request():
+    # 定制 HTTP 头
+    headers = {'User-Agent': 'Mozilla/5.0','x-my-header': 'my value'}
+    req = urllib2.Request('http://blog.kamidox.com',headers=headers)
+    # urlopen既可以接受网址为参数，也可以接受request对象为参数，后者可以定制请求头
+    s = urllib2.urlopen(req)
+    print s.read(100)
+    s.close()
+
 if __name__ == '__main__':
-    urlopen()
+    request()
