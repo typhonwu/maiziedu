56c9c7f8892272d0cd861bce7e4e42b13b6f2243
hughohoho <johnson_hugh@163.com>
2016-06-14 18:38:50 +0800

    爬虫基础-urllib2-捕获未获取异常

--- 5_spider_basic/urllib2_demo.py
+++ 5_spider_basic/urllib2_demo.py
@@ -6,9 +6,14 @@ urllib独有编码解码功能
 import urllib2
 
 def urlopen():
-    url = 'http://blog.kamidox.com'
-    s = urllib2.urlopen(url, timeout=3)
-    print s.read(100)
+    url = 'http://blog.kamidox.com/no_exist'
+    try:
+        s = urllib2.urlopen(url, timeout=3)
+    except urllib2.HTTPError,e:
+        print e
+    else:
+        print s.read(100)
+        s.close()
 
 if __name__ == '__main__':
     urlopen()
