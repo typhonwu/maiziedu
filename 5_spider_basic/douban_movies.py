9c8d672fc2e9751ad5b0b648b468757dd355420e
hughohoho <johnson_hugh@163.com>
2016-06-14 21:06:38 +0800

    爬虫基础-urllib2-豆瓣爬虫的解析器-完成解析目标属性值的内部方法_attr()

--- 5_spider_basic/douban_movies.py
+++ 5_spider_basic/douban_movies.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
-import urllib2
+import urllib
+from HTMLParser import HTMLParser
+
+
+class MovieParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.movies = []
+    # 重写这个方法来针对新解析喂入的html数据
+    def handle_startendtag(self,tag,attrs):
+        # 定义一个内部函数用来解析属性
+        def _attr(attrlist, attrname):  # 传入属性列表和要获取属性值的属性名
+            for attr in attrlist:  # 取出的attr是元组，0下标指属性名，1下标指向属性值
+                if attr[0] == attrname:
+                    return attr[1]
+            return None
+
 
 def nowplaying_movies(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)\
-                   AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73\
-                   Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36'}
     req = urllib2.Request(url, headers=headers)
     s = urllib2.urlopen(req)
-    parser = MovieParser() # 这是另外准备好的电影解析器
+    parser = MovieParser()  # 这是另外准备好的电影解析器
     parser.feed(s.read)  # 给解析器喂数据
     s.close()
     return parser.movies  # 解析器解析后返回解析好的电影
