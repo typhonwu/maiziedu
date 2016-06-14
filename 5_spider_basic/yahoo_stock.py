90b4fbd89f2f57e2fdc900b578636fbb68dbc3af
hughohoho <johnson_hugh@163.com>
2016-06-14 17:11:13 +0800

    爬虫基础-对urllib基础使用进行练习-雅虎财经获取某段时间股票数据

--- 5_spider_basic/yahoo_stock.py
+++ 5_spider_basic/yahoo_stock.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
 import urllib
+import datetime
 
-def download_stock_data(stock_list):
+def download_stock_data(stock_list):  # 下载全部记录
     # 对列表中的股票id一一获取记录
     for sid in stock_list:
         url = 'http://table.finance.yahoo.com/table.csv?s=' + sid
@@ -9,6 +10,22 @@ def download_stock_data(stock_list):
         print 'downloading %s from %s' % (fname,url)
         urllib.urlretrieve(url,fname)
 
+def download_stock_data_in_period(stock_list,start,end): # 下载指定时间段记录
+    for sid in stock_list:
+        params = {'a': start.month -1,'b': start.day,'c': start.year,
+               'd': end.month - 1,'e': end.day, 'f': end.year, 's':sid}
+        url = 'http://table.finance.yahoo.com/table.csv?s='
+        qs = urllib.urlencode(params)
+        url = url + qs
+        fname = '%s_%d%d%d_%d%d%d.csv' % (sid,start.year,start.month,start.day,
+                                         end.year,end.month,end.day)
+        print 'downloading %s from %s' % (fname,url)
+        urllib.urlretrieve(url,fname)
+
+
+
 if __name__ == '__main__':
     stock_list = ['300001.sz','300002.sz']
-    download_stock_data(stock_list)
+    end = datetime.date(year=2015,month=5,day=17)
+    start = datetime.date(year=2015,month=4,day=17)
+    download_stock_data_in_period(stock_list,start,end)
