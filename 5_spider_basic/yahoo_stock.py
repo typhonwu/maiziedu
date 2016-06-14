2893d7b18c4ca6e4e9d399da53c1f57354bdf3ec
hughohoho <johnson_hugh@163.com>
2016-06-14 16:59:55 +0800

    爬虫基础-对urllib基础使用进行练习-雅虎财经获取股票数据-优化一下打印下载进度

--- 5_spider_basic/yahoo_stock.py
+++ 5_spider_basic/yahoo_stock.py
@@ -5,7 +5,8 @@ def download_stock_data(stock_list):
     # 对列表中的股票id一一获取记录
     for sid in stock_list:
         url = 'http://table.finance.yahoo.com/table.csv?s=' + sid
-        fname = sid + '.csv' 
+        fname = sid + '.csv'
+        print 'downloading %s from %s' % (fname,url)
         urllib.urlretrieve(url,fname)
 
 if __name__ == '__main__':
