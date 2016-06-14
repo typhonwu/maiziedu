80a227ee62c59ad1250ba047af019e03543ef2c8
hughohoho <johnson_hugh@163.com>
2016-06-14 23:05:04 +0800

    爬虫基础-requests第三方库-发送参数进行查询

--- 5_spider_basic/requests_demo.py
+++ 5_spider_basic/requests_demo.py
@@ -15,5 +15,14 @@ def get_json():
     print(r.headers)
     # print(r.json())
 
+def get_querystring():
+    # 这个网址专门用于测试http协议
+    url = 'http://httpbin.org/get'
+    params = {'qs1': 'value1', 'qs2': 'value2'}
+    r = requests.get(url, params=params)
+    print(r.status_code)
+    print(r.content)
+
 if __name__ == '__main__':
-    get_json()
+    # get_json()
+    get_querystring()
