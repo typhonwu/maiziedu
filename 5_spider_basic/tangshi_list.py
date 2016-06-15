54a7d4a1ec689d340ac192dbf86cb4b720741dbd
hughohoho <johnson_hugh@163.com>
2016-06-15 13:55:37 +0800

    爬虫基础-正则-爬取唐诗三百首-解析器-设定标志确保找到目标标签

--- 5_spider_basic/tangshi_list.py
+++ 5_spider_basic/tangshi_list.py
@@ -14,12 +14,21 @@ def _attr(attrs, attrname):
 class PoemParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
+        # 默认未进入特定的标签，后面如果找到符合条件的标签再改为True,然后开始抓取
+        self.in_div = False
+        self.in_a = False
         self.tangshi_list = []
         self.current_poem = {}
         pass
 
     def handle_starttag(self, tag, attrs):
-        pass
+        # 如果找到目标标签内容，就把标志改为True
+        if tag == 'div' and _attr(attrs, 'class') == 'guwencont2':
+            self.in_div = True
+
+        if tag == 'a' and self.in_div:
+            self.in_a = True
+            self.current_poem['url'] = _attr(attrs, 'href')
 
     def handle_endtag(self, tag):
         pass
@@ -31,6 +40,7 @@ class PoemParser(HTMLParser):
 def retrive_tangshi_300():
     url = 'http://www.gushiwen.org/gushi/tangshi.aspx'
     r = requests.get(url)
+    # 专门解析唐诗的解析器
     parser = PoemParser()
     parser.feed(r.content)
     return parser.tangshi_list
