4142aeb6f216134d776cb461f5b895c61a449f96
hughohoho <johnson_hugh@163.com>
2016-06-15 17:35:33 +0800

    爬虫基础-线程-线性执行带线程锁的线程，每次只执行一个线程

--- 5_spider_basic/threading_demo.py
+++ 5_spider_basic/threading_demo.py
@@ -17,7 +17,20 @@ def simple_thread_demo():
         # 每个线程都执行worker_func这个方法，注意这里不需要带括号
         threading.Thread(target=worker_func).start()
 
+# 线程安全锁：一个时间只会有一个线程在运行
+def worker_func_lock(lock):
+    lock.acquire() # 请求
+    worker_func() # 执行
+    lock.release() # 释放
 
+# 线程锁
+gLock = threading.Lock()
+
+def thread_lock_demo():
+    for i in range(10):
+        # 开始带锁的线程
+        threading.Thread(target=worker_func_lock, args=[gLock]).start()
 
 if __name__ == '__main__':
-    simple_thread_demo()
+    # simple_thread_demo()
+    thread_lock_demo()
