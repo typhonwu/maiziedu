8d5387cbfbc2e1aa46eb88979a54957df639d1d2
hughohoho <johnson_hugh@163.com>
2016-06-15 17:26:17 +0800

    爬虫基础-线程-创建10个线程执行函数

--- 5_spider_basic/threading_demo.py
+++ 5_spider_basic/threading_demo.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 import threading
+import random
+import time
 
+def worker_func():
+    print('worker thread started in %s' % (threading.current_thread()))
+    # 改变随机数生成器的种子
+    random.seed()
+    # 让线程睡眠s随机一段时间
+    time.sleep(random.random())
+    print('worker thread finished in %s' % (threading.current_thread()))
 
 def simple_thread_demo():
-    pass
+    # 创建十个线程来进行测试
+    for i in range(10):
+        # 每个线程都执行worker_func这个方法，注意这里不需要带括号
+        threading.Thread(target=worker_func).start()
+
 
 
 if __name__ == '__main__':
