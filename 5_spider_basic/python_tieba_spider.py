c142d52f66d2109df2eef2e9b09fdc99945089e9
hughohoho <johnson_hugh@163.com>
2016-06-16 10:54:28 +0800

    爬虫基础-作业-爬取python贴吧用户名与头像-搭好框架

---
+++ 5_spider_basic/python_tieba_spider.py
@@ -0,0 +1,28 @@
+# -*- utf-8 -*-
+'''
+爬取python贴吧里面用户名及头像图片信息.爬取网页链接:http://tieba.baidu.com/f?kw=python&fr=ala0&tpl=5，只需要爬取该贴吧链接里面的头像即可,用户名作为头像图片的名称。
+'''
+
+class python_tieba_spider():
+    def __init(self):
+        self.url = 'http://tieba.baidu.com/f?kw=python&fr=ala0&tpl=5'
+        self.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:45.0) Gecko/20100101 Firefox/45.0'
+
+
+    def get_page(self):
+        pass
+
+    def parse(self,content):
+        pass
+
+    def save(self):
+        pass
+
+    def run(self):
+        print('开始抓取python贴吧用户名及图片信息...')
+        content = self.get_page(self.url)
+        items = self.parse(content)
+        self.save(items,'python_tieba_users')
+if __name__ == '__main__'
+    pts = python_tiea_spider()
+    pts.run()
