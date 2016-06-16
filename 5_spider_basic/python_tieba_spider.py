7ae8ed1b9f0e89f6da5465f7d50ad02ac27dfc87
hughohoho <johnson_hugh@163.com>
2016-06-16 14:38:42 +0800

    爬虫基础-作业-爬取python贴吧用户名与头像-解析标签-step1-确定数据所在div标签

--- 5_spider_basic/python_tieba_spider.py
+++ 5_spider_basic/python_tieba_spider.py
@@ -2,6 +2,36 @@
 '''
 爬取python贴吧里面用户名及头像图片信息.爬取网页链接:http://tieba.baidu.com/f?kw=python&fr=ala0&tpl=5，只需要爬取该贴吧链接里面的头像即可,用户名作为头像图片的名称。
 '''
+
+
+'''
+要获取的html标签范围：
+<div class="threadlist_lz clearfix"> # 这是第一个定位标签
+
+                <div class="threadlist_title pull_left j_th_tit ">
+    
+    
+    <a href="/p/4612057728" title="【腾讯课堂免费Python直播课程】秒杀一切Python视频资料" target="_blank" class="j_th_tit ">【腾讯课堂免费Python直播课程】秒杀一切Python视频资料</a>
+</div><div class="threadlist_author pull_right">
+
+
+    # 这是第二个定位标签，其中title是作者
+
+
+    <span class="tb_icon_author " title="主题作者: openlabczx" data-field="{&quot;user_id&quot;:1087985062}">
+        <i class="icon_author"></i>
+
+
+        # 这是最后一个定位标签，其中href就是用户首页，可以获取头像，注意要拼接一下
+
+
+        <a data-field="{&quot;un&quot;:&quot;openlabczx&quot;}" class="frs-author-name j_user_card " href="/home/main/?un=openlabczx&amp;ie=utf-8&amp;fr=frs" target="_blank">openlabczx</a>
+        <span class="icon_wrap  icon_wrap_theme1 frs_bright_icons "></span>    
+    </span>
+    <span class="pull-right is_show_create_time" title="创建时间">6-15</span>
+</div>
+            </div>
+'''
 import requests
 import re
 from HTMLParser import HTMLParser
@@ -12,7 +42,9 @@ class UserParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.user_list = []
+        self.in_div = False
         self.in_span = False
+        self.in_a = False
         self.current_user = {}
 
     def handle_starttag(self,tag,attrs):
@@ -22,17 +54,16 @@ class UserParser(HTMLParser):
                 if attr[0] == attrname:
                     return attr[1]
             return None
-        # 确认用户名所在的span标签
-        #pdb.set_trace()
-        if tag == 'span' and _attr(attrs, 'class') == 'tb_icon_author':
-            self.in_span = True
-            # 获取用户名属性  
-            self.current_user['name'] = _attr(attrs,'title')
+        # 确认用户名所在的div标签
+        if tag == 'div' and _attr(attrs, 'class') == 'threadlist_lz clearfix':
+            # pdb.set_trace()
+            print tag+':' + _attr(attrs, 'class')
+            self.in_div = True
             
 
 
 
-    def handle_data(self,data):
+    def handle_data(self, data):
         pass
 
 def retrieve_users():
@@ -40,7 +71,6 @@ def retrieve_users():
     r = requests.get(url)
     parser = UserParser()
     print chardet.detect(r.content)
-    
     parser.feed(r.content.decode('utf-8'))
     return parser.user_list
 
