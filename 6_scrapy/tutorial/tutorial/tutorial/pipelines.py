e1fa26b8ef7a526262293b845d10908f3d937721
hugh MBP <jonhson_hugh@163.com>
2016-06-19 10:47:20 +0800

    Scrapy-scrapy的基本使用步骤-保存数据到数据库-对pipeline的修改进行注释

--- 6_scrapy/tutorial/tutorial/tutorial/pipelines.py
+++ 6_scrapy/tutorial/tutorial/tutorial/pipelines.py
@@ -7,20 +7,24 @@
 
 
 import MySQLdb
-
-DBKWARGS={'db':'test','user':'root', 'passwd':'',
+# 这里把数据库的相关参数设置好
+DBKWARGS={'db':'test','user':'root', 'passwd':'123456',
     'host':'localhost','use_unicode':True, 'charset':'utf8'}
 
 class TutorialPipeline(object):
 
     def __init__(self):
         try:
+            # 传入设置好的参数与mysql建立连接
             self.con = MySQLdb.connect(**DBKWARGS)
         except Exception,e:
             print "Connect db error:",e
-        
+
+
     def process_item(self, item, spider):
+        # 通过连接建立游标，准备执行sql语句
         cur = self.con.cursor()
+        # sql语句中有三个占位符，对应后面替换的数据
         sql = "insert into dmoz_book values(%s,%s,%s)"
         lis = (''.join(item["title"]),''.join(item["link"]),
             ''.join(item["desc"]))
