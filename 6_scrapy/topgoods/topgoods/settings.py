1c38951f05f6b9d209ffb7caa2bac23173a40a92
hugh MBP <jonhson_hugh@163.com>
2016-06-20 20:44:10 +0800

    Scrapy-天猫数据抓取-图片抓取功能加入-图片下载还可以指定下载成什么尺寸

--- 6_scrapy/topgoods/topgoods/settings.py
+++ 6_scrapy/topgoods/topgoods/settings.py
@@ -20,7 +20,12 @@ NEWSPIDER_MODULE = 'topgoods.spiders'
 ITEM_PIPELINES = {'scrapy.pipelines.images.ImagesPipeline': 1}
 # 把items中的图片链接字段配置在这里
 IMAGES_URLS_FIELD = 'file_urls'
-# 设置图片保存路径
+# 设置图片保存路径,这个字段存的是原尺寸
 IMAGES_STORE = r'.'
-
+# 另外可以设置缩略图文件夹
+# 花括号里的字符串可以自定义，表示文件路径，然后带上图片尺寸
+IMAGES_THUMBS = {
+    'small': (50, 50),
+    'big': (270, 270),
+}
 LOG_FILE = "scrapy.log"
