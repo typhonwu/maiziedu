86b90d6eb903650c3a9cd7f542b752d95ae9df0f
hugh MBP <jonhson_hugh@163.com>
2016-06-20 16:31:28 +0800

    Scrapy-天猫数据抓取-图片抓取功能加入-settings中打开下载中间件，打开图片下载管道，设置一下图片保存路径

--- 6_scrapy/topgoods/topgoods/settings.py
+++ 6_scrapy/topgoods/topgoods/settings.py
@@ -12,6 +12,15 @@ BOT_NAME = 'topgoods'
 
 SPIDER_MODULES = ['topgoods.spiders']
 NEWSPIDER_MODULE = 'topgoods.spiders'
-
+# 打开下载中间件
+DOWNLOADER_MIDDLEWARES = {
+        'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware':301,
+    }
+# 打开图片下载管道
+ITEM_PIPELINES = {'scrapy.pipelines.images.ImagesPipeline': 1}
+# 把items中的图片链接字段配置在这里
+IMAGES_URLS_FIELD = 'file_urls'
+# 设置图片保存路径
+IMAGES_STORE = r'.'
 
 LOG_FILE = "scrapy.log"
