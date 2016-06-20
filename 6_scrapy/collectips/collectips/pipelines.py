8e63fc70019c3b3f9438b1a11b6d5201271b1bde
hugh MBP <jonhson_hugh@163.com>
2016-06-20 12:29:43 +0800

    Scrapy-西刺网ip抓取-从pipeline存入数据库-pipeline代码解读

--- 6_scrapy/collectips/collectips/pipelines.py
+++ 6_scrapy/collectips/collectips/pipelines.py
@@ -10,12 +10,16 @@ import MySQLdb
 class CollectipsPipeline(object):
 
     def process_item(self, item, spider):
-
+        # 从settings获取数据库相关参数DBKWARGS
         DBKWARGS = spider.settings.get('DBKWARGS')
+        # 使用获取的数据库参数建立连接
         con = MySQLdb.connect(**DBKWARGS)
+        # 获取数据库游标
         cur = con.cursor()
+        # 插入数据表的sql语句
         sql = ("insert into proxy(IP,PORT,TYPE,POSITION,SPEED,LAST_CHECK_TIME) "
             "values(%s,%s,%s,%s,%s,%s)")
+        # 占位符对应的数据
         lis = (item['IP'],item['PORT'],item['TYPE'],item['POSITION'],item['SPEED'],
             item['LAST_CHECK_TIME'])
         try:
