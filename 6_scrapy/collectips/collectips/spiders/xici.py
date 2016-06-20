d75322bb2158a65bfb2cc6075953e314b9e53eb5
hugh MBP <jonhson_hugh@163.com>
2016-06-20 12:25:50 +0800

    Scrapy-西刺网ip抓取-准备好西刺爬虫-对parse方法进行说明-xpath可以多次使用

--- 6_scrapy/collectips/collectips/spiders/xici.py
+++ 6_scrapy/collectips/collectips/spiders/xici.py
@@ -18,33 +18,33 @@ class XiciSpider(scrapy.Spider):
         for i in range(1, 206):
             req = scrapy.Request("http://www.xicidaili.com/nn/%s" % i)
             reqs.append(req)
-        
+
         return reqs
-    
+
+    # 每次发起请求获得服务器回应后就对response执行这个方法
     def parse(self, response):
-        ip_list=response.xpath('//table[@id="ip_list"]')
-        
+        ip_list = response.xpath('//table[@id="ip_list"]')
+        # 对获取的ip_list还可以再次使用xpath
         trs = ip_list[0].xpath('tr')
-        
-        items=[]
-        
+
+        items = []
+
         for ip in trs[1:]:
-            pre_item=CollectipsItem()
-            
+            pre_item = CollectipsItem()
+
             pre_item['IP'] = ip.xpath('td[3]/text()')[0].extract()
-            
+
             pre_item['PORT'] = ip.xpath('td[4]/text()')[0].extract()
-            
+
             pre_item['POSITION'] = ip.xpath('string(td[5])')[0].extract().strip()
-            
+
             pre_item['TYPE'] = ip.xpath('td[7]/text()')[0].extract()
-            
+
             pre_item['SPEED'] = ip.xpath(
                 'td[8]/div[@class="bar"]/@title').re('\d{0,2}\.\d{0,}')[0]
-                
+
             pre_item['LAST_CHECK_TIME'] = ip.xpath('td[10]/text()')[0].extract()
-            
+
             items.append(pre_item)
-            
+
         return items
-    
