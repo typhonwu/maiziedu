2cd5ced045840abfbf98476dd9b643a912c387bd
hugh MBP <jonhson_hugh@163.com>
2016-06-20 16:03:37 +0800

    Scrapy-天猫数据抓取-写好了spider-自带解析方法parse的注释-注意相对链接的处理

--- 6_scrapy/topgoods/topgoods/spiders/tm_goods.py
+++ 6_scrapy/topgoods/topgoods/spiders/tm_goods.py
@@ -2,35 +2,48 @@
 import scrapy
 from topgoods.items import TopgoodsItem
 
+
 class TmGoodsSpider(scrapy.Spider):
     name = "tm_goods"
     allowed_domains = ["http://www.tmall.com"]
     start_urls = (
         'http://list.tmall.com/search_product.htm?type=pc&totalPage=100&cat=50025135&sort=d&style=g&from=sn_1_cat-qp&active=1&jumpto=10#J_Filter',
     )
-    #记录处理的页数
-    count=0 
-     
+    # 记录处理的页数
+    count = 0
+
     def parse(self, response):
-          
+
         TmGoodsSpider.count += 1
-        
-        divs = response.xpath("//div[@id='J_ItemList']/div[@class='product']/div")
+
+        divs = response.xpath(
+            "//div[@id='J_ItemList']/div[@class='product']/div")
+        # 如果没有取到想要的元素就报错
         if not divs:
-            self.log( "List Page error--%s"%response.url )
-              
+            self.log("List Page error--%s" % response.url)
+
         for div in divs:
-            item=TopgoodsItem()
-            #商品价格
-            item["GOODS_PRICE"] = div.xpath("p[@class='productPrice']/em/@title")[0].extract()
-            #商品名称
-            item["GOODS_NAME"] = div.xpath("p[@class='productTitle']/a/@title")[0].extract()
-            #商品连接
-            pre_goods_url = div.xpath("p[@class='productTitle']/a/@href")[0].extract()
-            item["GOODS_URL"] = pre_goods_url if "http:" in pre_goods_url else ("http:"+pre_goods_url)
-            
-            yield scrapy.Request(url=item["GOODS_URL"],meta={'item':item},callback=self.parse_detail,
-            dont_filter=True)
+            item = TopgoodsItem()
+            # 商品价格
+            item["GOODS_PRICE"] = div.xpath(
+                "p[@class='productPrice']/em/@title")[0].extract()
+            # 商品名称
+            item["GOODS_NAME"] = div.xpath(
+                "p[@class='productTitle']/a/@title")[0].extract()
+            # 商品连接
+            pre_goods_url = div.xpath(
+                "p[@class='productTitle']/a/@href")[0].extract()
+
+            item["GOODS_URL"] = pre_goods_url \
+                # 如果不是绝对链接就拼接一下
+                    if "http:" in pre_goods_url \
+                    else ("http:" + pre_goods_url)
+
+            yield scrapy.Request(
+                url=item["GOODS_URL"],
+                meta={'item': item},
+                callback=self.parse_detail,
+                dont_filter=True)
 
     def parse_detail(self,response):
 
