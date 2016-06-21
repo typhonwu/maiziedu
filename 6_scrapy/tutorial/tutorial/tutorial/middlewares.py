5dc157c3036063d1500506f708a5baae70673dce
hugh MBP <jonhson_hugh@163.com>
2016-06-21 15:14:53 +0800

    Scrapy-代理ip的使用-proxymiddleware-注释理解

--- 6_scrapy/tutorial/tutorial/tutorial/middlewares.py
+++ 6_scrapy/tutorial/tutorial/tutorial/middlewares.py
@@ -6,7 +6,7 @@ import base64
 from proxy import GetIp,counter
 import logging
 
-
+# 首先获取高速可用的代理ip
 ips = GetIp().get_ips()
 
 
@@ -19,20 +19,21 @@ class ProxyMiddleware(object):
     # overwrite process request
     def process_request(self, request, spider):
         # Set the location of the proxy
+        # 对http链接处理
         if request.url.startswith("http://"):
             n = ProxyMiddleware.http_n
             n = n if n < len(ips['http']) else 0
             request.meta['proxy'] = "http://%s:%d" % (
-                ips['http'][n][0], int(ips['http'][n][1]))
+                ips['http'][n][0], int(ips['http'][n][1])) # 取出http可用的代理ip和端口
             logging.info(
                 'Squence - http: %s - %s' % (n, str(ips['http'][n])))
             ProxyMiddleware.http_n = n + 1
-
+        # 对https链接处理
         if request.url.startswith("https://"):
             n = ProxyMiddleware.https_n
             n = n if n < len(ips['https']) else 0         
             request.meta['proxy'] = "https://%s:%d" % (
-                ips['https'][n][0], int(ips['https'][n][1]))
+                ips['https'][n][0], int(ips['https'][n][1]))  # 取出https可用的代理ip和端口
             logging.info(
                 'Squence - https: %s - %s' % (n, str(ips['https'][n])))
             ProxyMiddleware.https_n = n + 1
