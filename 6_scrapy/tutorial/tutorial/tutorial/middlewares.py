9bb448ab3562f8a7d4577b1272352dbe56a0a551
hugh MBP <jonhson_hugh@163.com>
2016-06-21 15:09:02 +0800

    Scrapy-代理ip的使用-proxymiddleware-调整为pep8格式

--- 6_scrapy/tutorial/tutorial/tutorial/middlewares.py
+++ 6_scrapy/tutorial/tutorial/tutorial/middlewares.py
@@ -14,23 +14,25 @@ class ProxyMiddleware(object):
     # counter for http requests
     http_n = 0
     # counter for https requests
-    https_n = 0 
+    https_n = 0
 
     # overwrite process request
     def process_request(self, request, spider):
         # Set the location of the proxy
         if request.url.startswith("http://"):
             n = ProxyMiddleware.http_n
-            n = n if n<len(ips['http']) else 0 
-            request.meta['proxy']= "http://%s:%d"%(
-                ips['http'][n][0],int(ips['http'][n][1]))
-            logging.info('Squence - http: %s - %s'%(n,str(ips['http'][n])))
-            ProxyMiddleware.http_n=n+1
+            n = n if n < len(ips['http']) else 0
+            request.meta['proxy'] = "http://%s:%d" % (
+                ips['http'][n][0], int(ips['http'][n][1]))
+            logging.info(
+                'Squence - http: %s - %s' % (n, str(ips['http'][n])))
+            ProxyMiddleware.http_n = n + 1
 
         if request.url.startswith("https://"):
-            n=ProxyMiddleware.https_n
-            n=n if n<len(ips['https']) else 0             
-            request.meta['proxy']= "https://%s:%d"%(
-                ips['https'][n][0],int(ips['https'][n][1]))
-            logging.info('Squence - https: %s - %s'%(n,str(ips['https'][n])))
-            ProxyMiddleware.https_n=n+1 
+            n = ProxyMiddleware.https_n
+            n = n if n < len(ips['https']) else 0         
+            request.meta['proxy'] = "https://%s:%d" % (
+                ips['https'][n][0], int(ips['https'][n][1]))
+            logging.info(
+                'Squence - https: %s - %s' % (n, str(ips['https'][n])))
+            ProxyMiddleware.https_n = n + 1
