a5c91dd45bd41875281ad8fcd8ab4c6f15de0d17
hugh MBP <jonhson_hugh@163.com>
2016-06-22 01:08:54 +0800

    Scrapy入门作业-豆瓣电影爬虫-注意start_url是一个元组，里面一定要有逗号结尾

--- 6_scrapy/douban_movie/douban_movie/spiders/movie_scraper.py
+++ 6_scrapy/douban_movie/douban_movie/spiders/movie_scraper.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import pdb
 from douban_movie.items import DoubanMovieItem
 
 class movie_scraper(scrapy.Spider):
     name = 'douban_movie_spider'
-    allowed_domains = ["http:/www.douban.com"]
+    allowed_domains = ["douban.com"]
     start_url = (
-        'https://movie.douban.com/subject/10484117/?tag=热门&from=gaia'
+        'https://movie.douban.com/explore#!type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0',
     )
 
     def parse(self,response):
-
         a_list = response.xpath("//*[@id='gaia']/div[4]/div/a")
 
         if not a_list:
             self.log("List Page error-- %s" % response.url)
 
-        print a_list
