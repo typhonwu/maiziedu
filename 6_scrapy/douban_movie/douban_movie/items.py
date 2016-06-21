eafa85e9a03631c240e76a743a5b7e38800b86e1
hugh MBP <jonhson_hugh@163.com>
2016-06-21 17:33:29 +0800

    Scrapy入门作业-豆瓣电影爬虫-写好items

--- 6_scrapy/douban_movie/douban_movie/items.py
+++ 6_scrapy/douban_movie/douban_movie/items.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class DoubanMovieItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    intro = scrapy.Field()
+    post_url = scrapy.Field()
