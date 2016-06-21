330fdb384e70278a1181098cc9c75409a8ce20f2
hugh MBP <jonhson_hugh@163.com>
2016-06-21 17:43:11 +0800

    Scrapy入门作业-豆瓣电影爬虫-写了第一步获取电影列表并开启爬虫日志

---
+++ 6_scrapy/douban_movie/douban_movie/spiders/movie_scraper.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from douban_movie.items import DoubanMovieItem
+
+class movie_scraper(scrapy.Spider)
+    name = 'douban_movie_spider'
+    allowed_domains = ["http:/www.douban.com"]
+    start_url = (
+        'https://movie.douban.com/subject/10484117/?tag=热门&from=gaia'
+    )
+
+    def parse(self,response):
+
+        a_list = response.xpath("//*[@id="gaia"]/div[4]/div/a")
+
+        if not a_list:
+            self.log("List Page error-- %s" % response.url)
+
+        print a_list
