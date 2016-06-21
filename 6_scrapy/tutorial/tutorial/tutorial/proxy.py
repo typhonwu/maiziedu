4291074775054d8cd0630535f83977b6a72bbf72
hugh MBP <jonhson_hugh@163.com>
2016-06-21 13:19:02 +0800

    Scrapy-代理ip的使用-proxy-为获取ip专门写了个类-具体见注释，这里专门再次提一下列表解析式的简洁用法-调用时要记得使用selenium

--- 6_scrapy/tutorial/tutorial/tutorial/proxy.py
+++ 6_scrapy/tutorial/tutorial/tutorial/proxy.py
@@ -59,50 +59,61 @@ class Singleton(object):
             cls._instance = orig.__new__(cls, *args, **kw)  
         return cls._instance 
 
+# 这个类用于获取ip
+
 
 class GetIp(Singleton):
+    # 每次创建实例时就自动进行查询获取可用代理ip
     def __init__(self):
-        sql='''SELECT  `IP`,`PORT`,`TYPE`
-        FROM  `proxy` 
+        sql = '''SELECT  `IP`,`PORT`,`TYPE`
+        FROM  `proxy`
         WHERE `TYPE` REGEXP  'HTTP|HTTPS'
         AND  `SPEED`<5 OR `SPEED` IS NULL
-        ORDER BY `proxy`.`TYPE` ASC 
+        ORDER BY `proxy`.`TYPE` ASC
         LIMIT 50 '''
-        self.result = exec_sql(sql,**kwargs)
-    def del_ip(self,record):
+        self.result = exec_sql(sql, **kwargs)
+
+    # 把不能用的ip删掉
+    def del_ip(self, record):
         '''delete ip that can not use'''
-        sql="delete from proxy where IP='%s' and PORT='%s'"%(record[0],record[1])
+        sql = "delete from proxy where IP='%s' and PORT='%s'" % (record[0], record[1])
         print sql
-        exec_sql(sql,**kwargs)
-        print record ," was deleted."
-    def judge_ip(self,record):
+        exec_sql(sql, **kwargs)
+        print record, " was deleted."
+    
+    # 判断ip是否能用
+    def judge_ip(self, record):
         '''Judge IP can use or not'''
-        http_url="http://www.baidu.com/"
-        https_url="https://www.alipay.com/"
-        proxy_type=record[2].lower()
-        url=http_url if  proxy_type== "http" else https_url
-        proxy="%s:%s"%(record[0],record[1])
-        try:
-            req=urllib2.Request(url=url)
-            req.set_proxy(proxy,proxy_type)
-            response=urllib2.urlopen(req,timeout=30)
-        except Exception,e:
-            print "Request Error:",e
+        # 先分别对http和https进行处理
+        http_url = "http://www.baidu.com/"
+        https_url = "https://www.alipay.com/"
+        proxy_type = record[2].lower()
+        url = http_url if proxy_type == "http" else https_url
+        proxy = "%s:%s" % (record[0], record[1])
+        # 下面开始判断
+        try:  # 首先看是否超时，速度太慢不行
+            req = urllib2.Request(url=url)
+            req.set_proxy(proxy, proxy_type)
+            response = urllib2.urlopen(req, timeout=30)
+        except Exception, e:
+            print "Request Error:", e
             self.del_ip(record)
             return False
-        else:
-            code=response.getcode()
-            if code>=200 and code<300:
+        else:  # 然后看状态码是否正常，能否使用
+            code = response.getcode()
+            if code >= 200 and code < 300:
                 print 'Effective proxy',record
                 return True
             else:
                 print 'Invalide proxy',record
                 self.del_ip(record)
                 return False
-        
+    # 这是核心方法，这个类就是用来获取可用且高速的代理ip
     def get_ips(self):
         print "Proxy getip was executed."
-        http=[h[0:2] for h in self.result if h[2] =="HTTP" and self.judge_ip(h)]
+        # 这两个列表解析式用法很简洁
+        # 分别返回高速的http和https可用代理ip
+        http = [h[0:2] for h in self.result if h[2] =="HTTP" and self.judge_ip(h)]
         https=[h[0:2] for h in self.result if h[2] =="HTTPS" and self.judge_ip(h)]
         print "Http: ",len(http),"Https: ",len(https)
         return {"http":http,"https":https}
