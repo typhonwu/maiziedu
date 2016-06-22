68f8c64caf7d52ab727bbf584c1fa692247fbf0f
hugh MBP <jonhson_hugh@163.com>
2016-06-22 12:27:39 +0800

    Scrapy入门作业-豆瓣电影爬虫-注意链接中有些特别的地方要转义，否则不能识别

--- 6_scrapy/douban_movie/douban_movie/spiders/movie_scraper.py
+++ 6_scrapy/douban_movie/douban_movie/spiders/movie_scraper.py
@@ -7,12 +7,12 @@ class movie_scraper(scrapy.Spider):
     name = 'douban_movie_spider'
     allowed_domains = ["douban.com"]
     start_url = (
-        'https://movie.douban.com/explore#!type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0',
+        'https://movie.douban.com/explore#\!type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0',
     )
 
     def parse(self,response):
+        pdb.set_trace()
         a_list = response.xpath("//*[@id='gaia']/div[4]/div/a")
 
         if not a_list:
             self.log("List Page error-- %s" % response.url)
-
