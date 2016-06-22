24d96361c652f63e83f73f9aefd09c9fde9b25d9
hugh MBP <jonhson_hugh@163.com>
2016-06-22 14:20:05 +0800

    Scrapy入门作业-豆瓣电影爬虫-因为豆瓣反爬虫，需要加上headers，所以改用start_request()，返回的request对象默认获得相应后回调parse函数

--- 6_scrapy/douban_movie/douban_movie/spiders/movie_scraper.py
+++ 6_scrapy/douban_movie/douban_movie/spiders/movie_scraper.py
@@ -5,13 +5,28 @@ from douban_movie.items import DoubanMovieItem
 
 class movie_scraper(scrapy.Spider):
     name = 'douban_movie_spider'
+    # 设置headers，注意每行要加逗号
+    headers = {
+        'Accept':'*/*',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+        'Cache-Control':'max-age=0',
+        'Connection':'keep-alive',
+        # 暂时不用cookie试试。 'Cookie':'bid=yAMd84V94sg; ps=y; ll="118318"; ue="hughohoho@gmail.com"; dbcl2="113667643:Aku7Uxb2dSY"; ck=Hwp_; ap=1; __utma=30149280.743563175.1422940286.1466065258.1466501870.13; __utmc=30149280; __utmz=30149280.1465956799.11.1.utmcsr=accounts.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/login; __utmv=30149280.11366; __utma=223695111.1387345932.1466501870.1466501870.1466501870.1; __utmc=223695111; __utmz=223695111.1466501870.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); push_noty_num=0; push_doumail_num=4; _pk_id.100001.4cf6=f63938885dc79088.1466501870.2.1466574649.1466503191.; _pk_ses.100001.4cf6=*'
+        'Host':'movie.douban.com',
+        'Referer':'https://movie.douban.com/explore',
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
+        'X-Requested-With':'XMLHttpRequest',
+    }
+    pdb.set_trace()
     allowed_domains = ["douban.com"]
-    start_url = (
-        'https://movie.douban.com/explore#\!type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0',
-    )
 
+    def start_requests(self):
+        url = "https://movie.douban.com/explore#\!type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0"
+        return scrapy.Request(url = url, headers = headers)
+    
     def parse(self,response):
-        pdb.set_trace()
+        print response.url
         a_list = response.xpath("//*[@id='gaia']/div[4]/div/a")
 
         if not a_list:
