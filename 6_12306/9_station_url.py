4686ac9182b45f673405094fddfe260b9c40f62d
hugh MBP <jonhson_hugh@163.com>
2016-06-23 14:20:48 +0800

    12306爬虫-获取铁路局站点信息-获取站点url并发送请求获取站点信息

--- 6_12306/9_station_url.py
+++ 6_12306/9_station_url.py
@@ -5,6 +5,37 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
+def fetch_data(url, bureau, desc, fd):
+    try:
+        s = requests.get(url)
+    except Exception, e:
+        print "requests url fail. " + url
+        return
+
+    b = BeautifulSoup(s.content, "lxml")
+    datas = b.select("tbody > tr")
+    if len(datas) <= 2:
+        s = "find nothing " + url + " " + bureau.encode("utf-8") + " " + desc.encode("utf-8")
+        print s
+        fd.write(s + "\n")
+
+    for i in range(0, len(datas)):
+        if i < 2:
+            continue
+        infos = datas[i].find_all("td")
+        
+        out = u""
+        for info in infos:
+            out += info.text
+            out += u"， "
+
+        out += bureau + u"， " + desc
+        s = out.encode("utf-8")
+        fd.write(s)
+        fd.write("\n")
+        print s
+
+
 if __name__ == "__main__":
 
     url = "http://www.12306.cn/mormhweb/kyyyz/"
@@ -17,12 +48,19 @@ if __name__ == "__main__":
 
     b = BeautifulSoup(s.content, "lxml")
     names = b.select("#secTable > tbody > tr > td")
-    sub_urls = b.select("#mainTable td.submenu_bg > a")
 
-    for i in range(0, len(names)):
-        sub_url1 = sub_urls[i * 2]["href"]
-        sub_url2 = sub_urls[i * 2 + 1]["href"]
-        print names[i].text, sub_url1, sub_url2
+    sub_urls = b.select("#mainTable td.submenu_bg > a")
+    with open("9.2.txt", "w") as fd:
+        for i in range(0, len(names)):
+            sub_url1 = url + sub_urls[i * 2]["href"][2:]
+            print sub_url1
+            fetch_data(sub_url1, names[i].text, u"车站", fd)
+            time.sleep(5)
+
+            sub_url2 = url + sub_urls[i * 2 + 1]["href"][2:] 
+            print sub_url2
+            fetch_data(sub_url2, names[i].text, u"乘降所", fd)
+            time.sleep(5)
 
 
     
