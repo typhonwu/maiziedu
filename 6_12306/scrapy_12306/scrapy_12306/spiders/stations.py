d17f2dc8facb8529ae16fa8369fbafb99d3232c2
hugh MBP <jonhson_hugh@163.com>
2016-06-24 21:56:24 +0800

    12306爬虫-scrapy-重构获取站点信息-staionSpider-多级请求的使用

--- 6_12306/scrapy_12306/scrapy_12306/spiders/stations.py
+++ 6_12306/scrapy_12306/scrapy_12306/spiders/stations.py
@@ -22,13 +22,15 @@ class StationsSpider(scrapy.Spider):
         # 返回一个选择器，extract后返回字符串，是unicode编码
         names = response.css("#secTable > tbody > tr > td::text").extract()
         sub_urls = response.css("#mainTable td.submenu_bg > a::attr(href)").extract()
+        # 第一次请求站点页面之后再根据抓取的站点名称分别发出数据请求
+        # 第二次请求响应的是站点数据
         for i in range(0, len(names)):
             sub_url1 = response.url + sub_urls[i * 2][2:]
             yield Request(sub_url1, callback = self.parse_station, meta = {'bureau':names[i], 'station':True})
 
             sub_url2 = response.url + sub_urls[i * 2 + 1][2:]
             yield Request(sub_url2, callback = self.parse_station, meta = {'bureau':names[i], 'station':False})
-
+    # 对第二次请求得到的响应进行解析
     def parse_station(self, response):
         datas = response.css("table table tr")
         if len(datas) <= 2:
