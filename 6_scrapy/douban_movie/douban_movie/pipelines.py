f4f7314bb21fd93a47c52b0bd687dac511587016
hugh MBP <jonhson_hugh@163.com>
2016-06-24 10:04:37 +0800

    Scrapy入门作业-豆瓣电影爬虫-自定义imagepipeline来自定义图片保存路径-用print确定函数执行顺序-用公共变量传值

--- 6_scrapy/douban_movie/douban_movie/pipelines.py
+++ 6_scrapy/douban_movie/douban_movie/pipelines.py
@@ -1,18 +1,29 @@
+# -*- coding:utf-8 -*-
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exceptions import DropItem
 from scrapy.http import Request
 import pdb
+
+title = []
 class MyImagesPipeline(ImagesPipeline):
-    # 这里的request就是图片链接请求
-    def file_path(self, request, response=None, info= None):
-        pdb.set_trace()
-        image_guid = request.url.split('/')[-1]
-        return 'full/%s/%s' % (image_guid.split('.')[0], image_guid)
 
     def get_media_requests(self, item, info):
+        # print item['title']
+        global title
+        title = item['title']
         for image_url in item['post_urls']:
             yield Request(image_url)
 
+    # 这里的request就是图片链接请求
+    def file_path(self, request, response=None, info= None):
+        # print '这里执行的是file_path'
+        global title
+        print title
+        # image_guid = request.url.split('/')[-1]
+        return 'full/%s/%s' % (title, title+'.jpg')
+
+    # 这里的路径值来自于file_path
+    # 这里最后返回item
     def item_completed(self, results, item, info):
         image_paths = [x['path'] for ok, x in results if ok]
         if not image_paths:
