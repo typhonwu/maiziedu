22fae724c858cd37666f447b30e67b57c8262924
hugh MBP <jonhson_hugh@163.com>
2016-06-24 22:58:56 +0800

    12306爬虫-scrapy-获取列车时刻表-添加相应的pipeline

--- 6_12306/scrapy_12306/scrapy_12306/pipelines.py
+++ 6_12306/scrapy_12306/scrapy_12306/pipelines.py
@@ -73,3 +73,34 @@ class StationSQLPipeline(object):
                 item["name"], item["address"],
                 item["passenger"], item["luggage"],
                 item["package"]))
+
+class SQLPipeline(object):
+    def __init__(self):
+        self.conn = pymysql.connect(host = 'localhost', port = 3306, 
+                                        user = '12306',
+                                        password = '12306',
+                                        db = '12306-train',
+                                        charset = 'utf8')
+        self.cursor = self.conn.cursor()
+        self.brief_sql = "INSERT IGNORE INTO `train_briefs` VALUES\
+                    (%s, %s, %s, %s)"
+        self.info_sql = "INSERT IGNORE INTO `train_infos` VALUES\
+                    (%s, %s, %s, %s, %s, %s, %s)"
+
+    def process_item(self, item, spider):
+        try:
+            if isinstance(item, CommitItem):
+                self.conn.commit()
+            elif isinstance(item, BriefItem):
+                self.cursor.execute(self.brief_sql, (item["code"], 
+                    item["train_no"],
+                    item["start"], item["end"]))
+            else:
+                self.cursor.execute(self.info_sql, (item["train_no"], 
+                    item["no"],
+                    item["station"], item["type"],
+                    item["start_time"], item["arrive_time"],
+                    item["stopover_time"]))
+        except Exception, e:
+            spider.logger.warning("excute sql fail.")
+            spider.logger.warning(str(e))
