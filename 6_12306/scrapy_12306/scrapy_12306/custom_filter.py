ad43471345330201773791d29005c227ae687da0
hugh MBP <jonhson_hugh@163.com>
2016-06-25 17:07:30 +0800

    12306爬虫-scrapy-29_去重-注释自定义去重类

--- 6_12306/scrapy_12306/scrapy_12306/custom_filter.py
+++ 6_12306/scrapy_12306/scrapy_12306/custom_filter.py
@@ -3,25 +3,13 @@
 #import hashlib
 
 from scrapy.dupefilters import RFPDupeFilter
-
+# 通过查看源码知道只要继承RFPDupeFilter这个类就可以控制去重
 class CustomURLFilter(RFPDupeFilter):
+    # 这是生成指纹的方法，用来控制辨别重复的标准
     def request_fingerprint(self, request):
+        # 这里是以时间戳生成指纹
         if "timestamp" in request.meta:
             return request.url + "--" + request.meta["timestamp"]
         else:
             return request.url
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# vim: set ts=4 sw=4 sts=4 tw=100 et:
