d8424906a54a4efe1ff6939ef35aa0eaab02dbbf
hugh MBP <jonhson_hugh@163.com>
2016-06-25 18:41:19 +0800

    12306爬虫-scrapy-31_自定义下载中间件-对下载中间件注释

--- 6_12306/scrapy_12306/scrapy_12306/custom_middle.py
+++ 6_12306/scrapy_12306/scrapy_12306/custom_middle.py
@@ -6,13 +6,17 @@ from scrapy.exceptions import IgnoreRequest
 class CustomDownloaderMiddleware(object):
 
     def process_request(self, request, spider):
+        # 先打印日志，方便分析
         spider.logger.info("in midderware, " + request.url)
+        # 对expire值进行处理
         if "expire" in request.meta:
             s1 = request.meta["expire"]
             s2 = datetime.datetime.now()
+            # 如果超过了过期时间，就认为过期而丢弃请求
             if s1 < s2:
                 spider.logger.warning("in midderware, " + request.url + " expire.")
                 raise IgnoreRequest()
+            # 如果没有过期，就不处理
             else:
                 return None
 
